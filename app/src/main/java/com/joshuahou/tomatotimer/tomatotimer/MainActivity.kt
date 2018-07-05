0e47c17838f5fa74a246557d756d46bc05277334
jshou <josh@carbonfive.com>
2018-07-05 14:48:06 -0700

    working timer

--- app/src/main/java/com/joshuahou/tomatotimer/tomatotimer/MainActivity.kt
+++ app/src/main/java/com/joshuahou/tomatotimer/tomatotimer/MainActivity.kt
@@ -1,12 +1,54 @@
 package com.joshuahou.tomatotimer.tomatotimer
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.os.CountDownTimer
+import android.support.v7.app.AppCompatActivity
+import android.view.View
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+    companion object {
+        const val TOMATO_AMOUNT = (0.1 * 60 * 1000).toLong()
+        const val REST_AMOUNT = 5 * 60 * 1000
+    }
+
+    private var timer: CountDownTimer = object: CountDownTimer(TOMATO_AMOUNT, 1000) {
+        override fun onFinish() {
+            showFinishMessage()
+        }
+
+        override fun onTick(millisUntilFinished: Long) {
+            timerDisplay.text = formatTime(millisUntilFinished)
+        }
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    fun start(view: View) {
+        timer.start()
+        startButton.isClickable = false
+        Toast.makeText(this, getString(R.string.start_message), Toast.LENGTH_SHORT).show()
+    }
+
+    fun reset(view: View) {
+        timer.cancel()
+        startButton.isClickable = true
+        timerDisplay.text = formatTime(TOMATO_AMOUNT)
+        Toast.makeText(this, getString(R.string.reset_message), Toast.LENGTH_SHORT).show()
+    }
+
+    fun showFinishMessage() {
+        Toast.makeText(this, getString(R.string.finished_timer_message), Toast.LENGTH_SHORT).show()
+    }
+
+    fun formatTime(millis: Long): String {
+        val minutes = millis / 1000 / 60
+        val seconds = (millis / 1000 % 60).toString().padStart(2, '0')
+
+        return "$minutes:$seconds"
     }
 }
